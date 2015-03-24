bffba3edff0daf9c164702fb0272106a58e87eee
Denis Jean <denis.jean38@gmail.com>
2015-03-24 13:55:54 +0100

    Add a Fallback module

--- lib/void_logger.rb
+++ lib/void_logger.rb
@@ -3,7 +3,20 @@ require 'logger'
 class VoidLogger < ::Logger
   VERSION = "0.0.1"
 
+  def initialize(logdev=nil)
+    super(logdev) unless logdev.nil?
+  end
+
   def add(severity, message = nil, progname = nil, &block)
     return true
+  end
+
+  module Fallback
+    def logger
+      unless defined?(@_logger)
+        @_logger = (super rescue nil) || VoidLogger.new
+      end
+      @_logger
+    end
   end
 end
--- test/test_void_logger.rb
+++ test/test_void_logger.rb
@@ -1,8 +1,12 @@
 require 'minitest_helper'
+require 'logger'
+
+LOG_FILE_NAME = 'test/logfile.log'
+LOGGER = Logger.new LOG_FILE_NAME
 
 class TestVoidLogger < Minitest::Test
   def setup
-    @filepath = 'test/logfile.log'
+    @filepath = LOG_FILE_NAME
   end
 
   def teardown
@@ -16,4 +20,40 @@ class TestVoidLogger < Minitest::Test
     string = File.read(@filepath)
     assert !string.include?("Should not be logged")
   end
+
+  def test_fallback
+    # ancestors: [TestFallbackClass1, TestFallbackModule1, VoidLogger::Fallback, Object, Minitest::Expectations, Kernel, BasicObject]
+    t = TestFallbackClass1.new
+
+    assert t.logger.is_a?(VoidLogger)
+
+    # ancestors: [TestFallbackClass2, TestFallbackModule2, TestFallbackModule1, VoidLogger::Fallback, TestFallbackLogger, Object, Minitest::Expectations, Kernel, BasicObject]
+    t = TestFallbackClass2.new
+
+    assert_equal LOGGER, t.logger
+  end
+end
+
+module TestFallbackModule1
+  include VoidLogger::Fallback
+end
+
+class TestFallbackClass1
+  include TestFallbackModule1
+end
+
+module TestFallbackLogger
+  def logger
+    LOGGER
+  end
+end
+
+module TestFallbackModule2
+  include TestFallbackLogger
+  include VoidLogger::Fallback
+  include TestFallbackModule1
+end
+
+class TestFallbackClass2
+  include TestFallbackModule2
 end
